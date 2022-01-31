1f1a990cdeba86c6998be30c2b72ff29988dfc1b
nay-gupta <78629760+nay-gupta@users.noreply.github.com>
2022-01-31 17:22:36 -0500

    Get trucks from csv (#1)
    * Get Trucks from CSV
    
    * Add comments for explaination
    
    * Fix number of trucks added

---
+++ FoodTrucks.py
@@ -0,0 +1,9 @@
+from CSVReader import TruckReader
+
+class FoodTrucks:
+
+  def __init__(self, csvReader: TruckReader) -> None:
+    self.trucksByLine, self.idsByBlock, self.idsByLocation, self.count = csvReader.csvToTruckDicts()
+    self.count -= 1
+
+  
