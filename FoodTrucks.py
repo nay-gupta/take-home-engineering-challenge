690d8687d1eb37a730a9ddd1f4b9857408e02418
nay-gupta <naygupta97@gmail.com>
2022-01-31 22:29:06 -0500

    Change line endings

--- FoodTrucks.py
+++ FoodTrucks.py
@@ -6,4 +6,10 @@ class FoodTrucks:
     self.trucksByLine, self.idsByBlock, self.idsByLocation, self.count = csvReader.csvToTruckDicts()
     self.count -= 1
 
-  
+  def getTrucksByBlock(self, block) -> List:
+    if block in self.idsByBlock:
+      truckIds = self.idsByBlock
+      trucks = []
+      for id in truckIds:
+        trucks.append(self.trucksByLine[id])
+      return trucks
